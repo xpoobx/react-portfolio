528692f90048e6c987981eb4a19a7a8bc2755057
xpoobx <dantepellico@gmail.com>
2025-09-26 20:24:22 -0400

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function Home() {
 return (
