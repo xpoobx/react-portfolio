153522b38c49a5944667b048cfda74fdf34ca0e4
xpoobx <dantepellico@gmail.com>
2025-09-26 20:07:14 -0400

    Home & About Functions Empty

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Home() {
+return (
+<div className="home">
+<h1>Temp Text</h1>
+<p>Mission Statement:</p>
+<Link to="/about" className="btn">About Me</Link>
+</div>
+);
 }
+
+function About() {
+return (
+<div className="about">
+<h1>About Me</h1>
+<img src="/me.jpg" alt="My portrait" />
+<p>Intro Text</p>
+<a href="/resume.pdf" target="_blank" rel="noreferrer">Resume</a>
+</div>
+);
+}
+
+
 
 export default App
