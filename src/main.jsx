09398a397722b47e5cc4a91478e1b276c641f016
xpoobx <dantepellico@gmail.com>
2025-09-26 20:14:35 -0400

    Home & About Functions Empty2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
